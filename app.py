aa96265b32830264404db4add0b331a7293d1c0d
Zarar Siddiqi <zarars@gmail.com>
2018-12-18 20:57:32 -0500

    Startup and ports

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 import requests
+import os
 from flask import jsonify
 from flask_cors import CORS
 CORS(app)
@@ -21,3 +22,9 @@ def players():
 @app.route("/players/<player_id>/summary")
 def player_summary(player_id):
     return jsonify(requests.get('https://api.thescore.com/nba/players/' + player_id + '/summary').json())
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)   
