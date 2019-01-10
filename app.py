80e30d8ad53c6bd02f8d561ad19c89292519c436
Zarar Siddiqi <zarars@gmail.com>
2019-01-09 22:52:13 -0500

    Simplify startup, use conventional application name

--- app.py
+++ app.py
@@ -303,10 +303,8 @@ def encode_string(to_encode):
 def decode_string(to_decode):
     return base64.b64decode(to_decode.encode('utf-8')).decode()
 
-def start():
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)   
+application = app
 
 if __name__ == '__main__':
-    start()
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0')   
