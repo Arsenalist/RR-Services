9d8b4b2aabc4a3962220de2f6969c40b27be6826
Zarar Siddiqi <zarars@gmail.com>
2019-01-13 02:34:39 -0500

    Use the port.

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ application = app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    application.run(debug=True, host='0.0.0.0')   
+    application.run(debug=True, host='0.0.0.0', port=port)   
